35a9f1cdd778625a31d2d93a61c19fb4359434d0
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-05-17 15:29:03 -0300

    setup

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,9 +6,22 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 
-    :focus{
+   
+}
+
+:focus{
         outline: 0;
         box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
-    }
 }
+
+body{
+    background: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button{
+    font-family: 400 1rem Roboto, sans-serif;
+}
+
 `
