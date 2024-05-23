3517ba8dab8cc3f5dd94729f0f331f235767afd9
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-05-23 13:50:59 -0300

    using useContextSelector

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -10,7 +10,7 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as z from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -22,7 +22,13 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
+
   const {
     control,
     register,
@@ -35,10 +41,8 @@ export function NewTransactionModal() {
       type: 'income',
     },
   })
-
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
     const { description, price, category, type } = data
-
     await createTransaction({
       description,
       price,
